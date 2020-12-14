e51066eadc19b8f5083063906c121f15cf974c5e
bia-programming <bia.pasca@yahoo.com>
2020-12-14 19:36:35 +0200

    Rename file

--- src/main/java/com/example/spring2/BookNotFoundAdvice.java
+++ src/main/java/com/example/spring2/BookNotFoundAdvice.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-class BookNotFoundAdvice {
+public class BookNotFoundAdvice {
 
     @ResponseBody
     @ExceptionHandler(BookNotFoundException.class)
