9c7c58350d322550f0829ec32cfcb9869f1264a0
bia-programming <bia.pasca@yahoo.com>
2020-12-17 19:19:37 +0200

    .

--- src/main/java/com/example/spring2/SecurityConfiguration.java
+++ src/main/java/com/example/spring2/SecurityConfiguration.java
@@ -16,7 +16,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
